347cb39b6bf7be08a916351fe14dafb36387d7e4
chuong.dao <riochuong@gmail.com>
2023-08-26 14:56:07 -0700

    uncommented tests

--- tests/test_server.cpp
+++ tests/test_server.cpp
@@ -23,31 +23,31 @@ bool WaitForRecvWriteMessage(int num_trials, std::string_view message, uint32_t 
 }
 
 
-// TEST_CASE("Test PING Command","Test Mock Server"){
+TEST_CASE("Test PING Command","Test Mock Server"){
 
-//     MockAsyncReadWrite::setMessageToRead("*1\r\n$4\r\nPING\r\n"s);     
-//     auto mock_server = MockServer(1234, 5, false);
-//     mock_server.Run();
-//     int num_trials = 2;
-//     REQUIRE(WaitForRecvWriteMessage(2, "*1\r\n$4\r\nPONG\r\n"s, 1));    
-// }
+    MockAsyncReadWrite::setMessageToRead("*1\r\n$4\r\nPING\r\n"s);     
+    auto mock_server = MockServer(1234, 5, false);
+    mock_server.Run();
+    int num_trials = 2;
+    REQUIRE(WaitForRecvWriteMessage(2, "*1\r\n$4\r\nPONG\r\n"s, 1));    
+}
 
-// TEST_CASE("Test ECHO command basic", "Test Mock Server"){
+TEST_CASE("Test ECHO command basic", "Test Mock Server"){
 
-//     MockAsyncReadWrite::setMessageToRead("*2\r\n$4\r\nECHO\r\n$5\r\nTest1\r\n"s);     
-//     auto mock_server = MockServer(1234, 5, false);
-//     mock_server.Run();
-//     REQUIRE(WaitForRecvWriteMessage(2, "*1\r\n$5\r\nTest1\r\n"s, 1));    
-// }
+    MockAsyncReadWrite::setMessageToRead("*2\r\n$4\r\nECHO\r\n$5\r\nTest1\r\n"s);     
+    auto mock_server = MockServer(1234, 5, false);
+    mock_server.Run();
+    REQUIRE(WaitForRecvWriteMessage(2, "*1\r\n$5\r\nTest1\r\n"s, 1));    
+}
 
 
-// TEST_CASE("Test ECHO command long", "Test Mock Server"){
+TEST_CASE("Test ECHO command long", "Test Mock Server"){
 
-//     MockAsyncReadWrite::setMessageToRead("*2\r\n$4\r\nECHO\r\n$20\r\nTest12345678910 awsn\r\n"s);     
-//     auto mock_server = MockServer(1234, 5, false);
-//     mock_server.Run();
-//     REQUIRE(WaitForRecvWriteMessage(2, "*1\r\n$20\r\nTest12345678910 awsn\r\n"s, 1));    
-// }
+    MockAsyncReadWrite::setMessageToRead("*2\r\n$4\r\nECHO\r\n$20\r\nTest12345678910 awsn\r\n"s);     
+    auto mock_server = MockServer(1234, 5, false);
+    mock_server.Run();
+    REQUIRE(WaitForRecvWriteMessage(2, "*1\r\n$20\r\nTest12345678910 awsn\r\n"s, 1));    
+}
 
 // ======= INTEGRATED TESTING WITH REAL SERVER ===================================
 using RealRedisServer =  RedisServer::Server<asio::ip::tcp::acceptor, 
@@ -68,68 +68,68 @@ std::shared_ptr<RealRedisServer> StartRealRedisServer(uint16_t port_number, std:
     return server;
 }
 
-// TEST_CASE("Test PING Command with real server", "Test Real Server"){
-//     uint16_t port_number = 5551;
-//     std::string server_addr = "127.0.0.1"; // localhost address
-//     logger::info("ready for client !!!!!");
-//     TestClient tc {port_number, server_addr};
+TEST_CASE("Test PING Command with real server", "Test Real Server"){
+    uint16_t port_number = 5551;
+    std::string server_addr = "127.0.0.1"; // localhost address
+    logger::info("ready for client !!!!!");
+    TestClient tc {port_number, server_addr};
 
-//     auto server = StartRealRedisServer(port_number, server_addr); 
+    auto server = StartRealRedisServer(port_number, server_addr); 
 
-//     REQUIRE(tc.Connect());
-//     REQUIRE(tc.Send("*1\r\n$4\r\nPING\r\n"s));
-//     std::string recv = "";
-//     REQUIRE(tc.Recv(recv));
-//     REQUIRE(recv == "*1\r\n$4\r\nPONG\r\n"s);
-//     RedisServer::RespProtocol::ParserError ec = RedisServer::RespProtocol::ParserError::kOk;
-//     auto cmds = RedisServer::RespProtocol::CommandParser::Parse(recv, ec);
-//     REQUIRE(ec == RedisServer::RespProtocol::ParserError::kOk);
-//     REQUIRE(cmds.size() == 1);
-//     REQUIRE(cmds[0] == "PONG");
-//     server->Stop();
-// }
+    REQUIRE(tc.Connect());
+    REQUIRE(tc.Send("*1\r\n$4\r\nPING\r\n"s));
+    std::string recv = "";
+    REQUIRE(tc.Recv(recv));
+    REQUIRE(recv == "*1\r\n$4\r\nPONG\r\n"s);
+    RedisServer::RespProtocol::ParserError ec = RedisServer::RespProtocol::ParserError::kOk;
+    auto cmds = RedisServer::RespProtocol::CommandParser::Parse(recv, ec);
+    REQUIRE(ec == RedisServer::RespProtocol::ParserError::kOk);
+    REQUIRE(cmds.size() == 1);
+    REQUIRE(cmds[0] == "PONG");
+    server->Stop();
+}
 
-// TEST_CASE("Test basic short ECHO Command with real server", "Test Real Server"){
-//     uint16_t port_number = 5552;
-//     std::string server_addr = "127.0.0.1"; // localhost address
-//     logger::info("ready for client !!!!!");
-//     TestClient tc {port_number, server_addr};
+TEST_CASE("Test basic short ECHO Command with real server", "Test Real Server"){
+    uint16_t port_number = 5552;
+    std::string server_addr = "127.0.0.1"; // localhost address
+    logger::info("ready for client !!!!!");
+    TestClient tc {port_number, server_addr};
 
-//     auto server = StartRealRedisServer(port_number, server_addr); 
+    auto server = StartRealRedisServer(port_number, server_addr); 
 
-//     REQUIRE(tc.Connect());
-//     REQUIRE(tc.Send("*2\r\n$4\r\nECHO\r\n$5\r\n1234c\r\n"s));
-//     std::string recv = "";
-//     REQUIRE(tc.Recv(recv));
-//     REQUIRE(recv == "*1\r\n$5\r\n1234c\r\n"s);
-//     RedisServer::RespProtocol::ParserError ec = RedisServer::RespProtocol::ParserError::kOk;
-//     auto cmds = RedisServer::RespProtocol::CommandParser::Parse(recv, ec);
-//     REQUIRE(ec == RedisServer::RespProtocol::ParserError::kOk);
-//     REQUIRE(cmds.size() == 1);
-//     REQUIRE(cmds[0] == "1234c");
-//     server->Stop();
-// }
+    REQUIRE(tc.Connect());
+    REQUIRE(tc.Send("*2\r\n$4\r\nECHO\r\n$5\r\n1234c\r\n"s));
+    std::string recv = "";
+    REQUIRE(tc.Recv(recv));
+    REQUIRE(recv == "*1\r\n$5\r\n1234c\r\n"s);
+    RedisServer::RespProtocol::ParserError ec = RedisServer::RespProtocol::ParserError::kOk;
+    auto cmds = RedisServer::RespProtocol::CommandParser::Parse(recv, ec);
+    REQUIRE(ec == RedisServer::RespProtocol::ParserError::kOk);
+    REQUIRE(cmds.size() == 1);
+    REQUIRE(cmds[0] == "1234c");
+    server->Stop();
+}
 
-// TEST_CASE("Test basic long ECHO Command with real server", "Test Real Server"){
-//     uint16_t port_number = 5553;
-//     std::string server_addr = "127.0.0.1"; // localhost address
-//     logger::info("ready for client !!!!!");
-//     TestClient tc {port_number, server_addr};
+TEST_CASE("Test basic long ECHO Command with real server", "Test Real Server"){
+    uint16_t port_number = 5553;
+    std::string server_addr = "127.0.0.1"; // localhost address
+    logger::info("ready for client !!!!!");
+    TestClient tc {port_number, server_addr};
 
-//     auto server = StartRealRedisServer(port_number, server_addr); 
+    auto server = StartRealRedisServer(port_number, server_addr); 
 
-//     REQUIRE(tc.Connect());
-//     REQUIRE(tc.Send("*2\r\n$4\r\nECHO\r\n$21\r\n12345678901234567890c\r\n"s));
-//     std::string recv = "";
-//     REQUIRE(tc.Recv(recv));
-//     REQUIRE(recv == "*1\r\n$21\r\n12345678901234567890c\r\n"s);
-//     RedisServer::RespProtocol::ParserError ec = RedisServer::RespProtocol::ParserError::kOk;
-//     auto cmds = RedisServer::RespProtocol::CommandParser::Parse(recv, ec);
-//     REQUIRE(ec == RedisServer::RespProtocol::ParserError::kOk);
-//     REQUIRE(cmds.size() == 1);
-//     REQUIRE(cmds[0] == "12345678901234567890c");
-//     server->Stop();
-// }
+    REQUIRE(tc.Connect());
+    REQUIRE(tc.Send("*2\r\n$4\r\nECHO\r\n$21\r\n12345678901234567890c\r\n"s));
+    std::string recv = "";
+    REQUIRE(tc.Recv(recv));
+    REQUIRE(recv == "*1\r\n$21\r\n12345678901234567890c\r\n"s);
+    RedisServer::RespProtocol::ParserError ec = RedisServer::RespProtocol::ParserError::kOk;
+    auto cmds = RedisServer::RespProtocol::CommandParser::Parse(recv, ec);
+    REQUIRE(ec == RedisServer::RespProtocol::ParserError::kOk);
+    REQUIRE(cmds.size() == 1);
+    REQUIRE(cmds[0] == "12345678901234567890c");
+    server->Stop();
+}
 
 
 TEST_CASE("Test basic PING Command with redis cli", "Test Real Server RedisCLI"){
